aa558285b6ced892d345772949de6700dae77e93
Mr. Fleming <efleming@stpatrick.org>
2017-08-30 09:59:08 -0500

    added variables

---
+++ name.js
@@ -0,0 +1,18 @@
+let money =8;
+console.log(money+1);
+console.log(money+3);
+console.log(money+6);
+console.log(money+8);
+
+
+//command pallet
+//ctrl+shift+p   then script
+//ctrl+shift+b
+
+
+function mytest(){
+    if(){}
+}
+
+
+mytest();
