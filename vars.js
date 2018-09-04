663f3b40efd94270ab6c1cfcefb86bc795e02605
Mr. Fleming <efleming@stpatrick.org>
2018-09-04 14:51:18 -0500

    sdfasdfasdfasdf

---
+++ vars.js
@@ -0,0 +1,25 @@
+let a = 10;
+let b = -25;
+//create two more variables c and d and give them whatever number you want.
+let c = 24;
+let d = 34;
+
+const total = a+b+c+d;
+console.log(total);
+
+
+
+
+//new section on string variables
+let convo = "hello ";
+//set name to your name, first and last.
+let name;
+//add string convo and name together.
+const greeting =
+console.log(greeting);
+
+//make your own message similar to what is above.
+let convo2;
+let
+const
+console.log();
